b8c40702ffe6b18353050bcf331b4fde4703a616
lyquest <73520626+lyquest@users.noreply.github.com>
2020-11-22 21:00:08 +0800

    更新小窝助力

--- jd_small_home.js
+++ jd_small_home.js
@@ -56,7 +56,7 @@ if ($.isNode()) {
 }
 
 const JD_API_HOST = 'https://lkyl.dianpusoft.cn/api';
-const inviteCodes = ['1330276866033504257','1330482328716136449'];
+const inviteCodes = ['1330276866033504257'];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
