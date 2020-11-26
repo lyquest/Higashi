df06228341d8dc967e57853f2e3e37eb06d8241e
lyquest <73520626+lyquest@users.noreply.github.com>
2020-11-27 02:00:04 +0800

    更新

--- jdCookie.js
+++ jdCookie.js
@@ -1,31 +1,33 @@
 /*
-京东种豆得豆互助码
 此文件为Node.js专用。其他用户请忽略
-支持京东N个账号
  */
-//云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
-// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PLANT_BEAN_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
-// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
-// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let shareCodes = [
-  '',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '',//账号二的好友shareCode，不同好友中间用@符号隔开
+//此处填写京东账号cookie。
+//注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
+let CookieJDs = [
+  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
-// 判断github action里面是否有东东萌宠互助码
-if (process.env.DDFACTORY_SHARECODES) {
-  if (process.env.DDFACTORY_SHARECODES.indexOf('&') > -1) {
-    console.log(`您的互助码选择的是用&隔开\n`)
-    shareCodes = process.env.DDFACTORY_SHARECODES.split('&');
-  } else if (process.env.DDFACTORY_SHARECODES.indexOf('\n') > -1) {
-    console.log(`您的互助码选择的是用换行隔开\n`)
-    shareCodes = process.env.DDFACTORY_SHARECODES.split('\n');
+// 判断github action里面是否有京东ck
+if (process.env.JD_COOKIE) {
+  if (process.env.JD_COOKIE.indexOf('&') > -1) {
+    console.log(`您的cookie选择的是用&隔开\n`)
+    CookieJDs = process.env.JD_COOKIE.split('&');
+  } else if (process.env.JD_COOKIE.indexOf('\n') > -1) {
+    console.log(`您的cookie选择的是用换行隔开\n`)
+    CookieJDs = process.env.JD_COOKIE.split('\n');
+  } else if (process.env.JD_COOKIE.indexOf('\\n') > -1) {
+    //环境变量兼容腾讯云和docker下\n会被转义成\\n
+    console.log(`您的cookie选择的是用换行隔开\\n`)
+    CookieJDs = process.env.JD_COOKIE.split('\\n');
   } else {
-    shareCodes = process.env.DDFACTORY_SHARECODES.split();
+    CookieJDs = [process.env.JD_COOKIE];
   }
-} else if (process.env.JD_COOKIE) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  CookieJDs = [...new Set(CookieJDs)]
+  console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
+  console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
+  // console.log(`\n==================脚本执行来自 github action=====================\n`)
 }
-for (let i = 0; i < shareCodes.length; i++) {
+for (let i = 0; i < CookieJDs.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['shareCodes' + index] = shareCodes[i];
+  exports['CookieJD' + index] = CookieJDs[i];
 }
