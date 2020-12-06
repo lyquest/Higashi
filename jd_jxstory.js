fbd084783b441413f418a1dc28b967145145b0b4
lyquest <73520626+lyquest@users.noreply.github.com>
2020-12-06 20:09:53 +0800

    Update jd_jxstory.js

--- jd_jxstory.js
+++ jd_jxstory.js
@@ -31,7 +31,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = 3;
 let cookiesArr = [], cookie = '', message = '';
-const inviteCodes = ['qSDHMwUOz7onHcMyaju4KmdSXWf0dlv7LVnTt1Wzemo='];
+const inviteCodes = ['K-ZGp93cjZftvi-R7WV_rY9DJtXY6pehCM1m3zLR7EE='];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
