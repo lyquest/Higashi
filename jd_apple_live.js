4b326643bd468f4e9d5c19bcd0b14309842462d1
lyquest <73520626+lyquest@users.noreply.github.com>
2020-12-09 22:05:57 +0800

    Add files via upload

--- jd_apple_live.js
+++ jd_apple_live.js
@@ -38,7 +38,7 @@ if ($.isNode()) {
   cookiesArr.push(...[$.getdata('CookieJD'), $.getdata('CookieJD2')]);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [`P04z54XCjVUm4aW5nJcXCCyoR8C6s-kRmWs@P04z54XCjVUm4aW5m9cZ2bx3y5Ow@P04z54XCjVUm4aW5u2ak7ZCdan1BeYMuZ9HwF34gJjW@P04z54XCjVUm4aW5m9cZ2T6jChKkkjZEdhiKUY`, `P04z54XCjVUm4aW5nJcXCCyoR8C6s-kRmWs@P04z54XCjVUm4aW5m9cZ2bx3y5Ow`];
+const inviteCodes = [`P04z54XCjVUm4aW5nBnDmKr33lCkvEjdgXb4w@P04z54XCjVUm4aW5jQICmf4339KnClPZw`];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
